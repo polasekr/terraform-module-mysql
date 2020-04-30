deaa26582ccfb7a45a4e6d61785c98b143883e81
Robert Polasek <robert.polasek@toptal.com>
2020-04-30 11:23:09 -0400

    updated for terraform.io

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rgTerraformStateEastus"
-    storage_account_name = "stjudecloudtfstates"
-    container_name       = "core-eastus"
-    key                  = "operator/mysql-terraform.tfstate"
-  }
-}
